c85492ffa8ee584ca5d9241aa82ad55914071f7f
Florian_Th <florian.thom@student.htw-berlin.de>
2020-05-18 18:08:47 +0200

    Installed most of prob needed packages

---
+++ src/app.ts
@@ -0,0 +1,66 @@
+//import express from "express"; 
+// if above is not supported by your project environment then follow as below
+import * as express from "express";
+
+
+const app = express();
+
+app.get("/", (req, res)=> {
+  res.send("Hello");
+});
+
+app.listen(5000, () => console.log("Server running"));
+
+
+/* const http = require('http');;
+const express = require('express');
+const bodyParser = require('body-parser');
+// DB Config
+const db = require("./config/keys").PORT;
+
+
+
+
+
+const app = express();
+
+// Bodyparser Middleware
+// Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
+app.use(bodyParser.json());
+
+
+
+
+// conncect to mongo
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log("MongoDB connected..."))
+    .catch(err => console.log(err));
+
+// Use routes
+app.use("/api/pages", pages)
+app.use("/api/tests", tests)
+app.use("/api/searches", searches)
+app.use("/api/statistics", statistics)
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server started on port " + port));
+
+
+
+
+
+
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); */
---
+++ src/config/env.config.ts
@@ -0,0 +1,27 @@
+import * as env from 'env-var';
+const SERVER_PORT = env.get('SERVER_PORT').asPortNumber();
+
+/* const TOKEN_SECRET = env.get('TOKEN_SECRET').asString();
+const TOKEN_EXPIRY_TIME = env.get('TOKEN_EXPIRY_TIME').asString();
+const EMAIL_SERVICE = env.get('EMAIL_SERVICE').asString();
+const EMAIL_PORT = env.get('EMAIL_PORT').asPortNumber();
+const EMAIL_CIPHERS = env.get('EMAIL_CIPHERS').asString();
+const EMAIL_REJECT = env.get('EMAIL_REJECT').asBool();
+const USER_EMAIL = env.get('USER_EMAIL').asString();
+const PASSWORD_EMAIL = env.get('PASSWORD_EMAIL').asString();
+const EMAIL_ADDRESS = env.get('EMAIL_ADDRESS').asString();
+const VERIFICATIONCODELENGTH = env.get('VERIFICATIONCODELENGTH').asIntPositive(); */
+
+export {
+    SERVER_PORT,
+/*     TOKEN_SECRET,
+    TOKEN_EXPIRY_TIME,
+    EMAIL_ADDRESS,
+    EMAIL_CIPHERS,
+    EMAIL_PORT,
+    EMAIL_REJECT,
+    EMAIL_SERVICE,
+    USER_EMAIL,
+    PASSWORD_EMAIL,
+    VERIFICATIONCODELENGTH */
+};
